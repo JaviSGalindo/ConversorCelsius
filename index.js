81397c5dfe5014af0d6d0b91d564a55e2b06faca
JaviSGalindo <javier2006galindo@gmail.com>
2025-02-10 16:10:50 -0500

    ejercicio terminado, ajustes de css

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const inputCelsius = document.getElementById("celsius");
+const inputNumero = document.getElementById("numero");
 const inputSubmit = document.getElementById("convert");
-const celsiusShow = document.getElementById("celsiusValue");
-const fahrenheit = document.getElementById("fahrenheit");
-const kelvin = document.getElementById("kelvin");
+const factorialValue = document.getElementById("factorialValue");
 
 inputSubmit.addEventListener('click', function (submit) {
     submit.preventDefault();
 
-    const celsiusValue = parseFloat(inputCelsius.value);
-
-    if (isNaN(celsiusValue)) {
+    const number = parseFloat(inputNumero.value);
+    let resultado = 1;
+    
+    if (isNaN(resultado)) {
         alert("Por favor, ingresa un valor numérico válido.");
         return;
     }
-    
-    const fahrenheitValue = (celsiusValue * 9/5) + 32;
-    const kelvinValue = celsiusValue + 273.15;
-
-    celsiusShow.textContent = `${celsiusValue} °C`;
-    fahrenheit.textContent = `${fahrenheitValue.toFixed(2)} °F`;
-    kelvin.textContent = `${kelvinValue.toFixed(2)} °K`;
-    inputCelsius.value = "";
-});
+    else {
+        for (let i=2; i<=number; i++){
+            resultado = i * resultado;
+        }
+    }
+    factorialValue.textContent = resultado;
+    inputNumero.value = "";
+}); 
